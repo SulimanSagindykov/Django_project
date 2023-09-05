73a071742e18a006407641866c811cec8fece506
Suliman Sagindykov <sulimansagindykov@gmail.com>
2023-09-05 11:41:22 +0600

    home_page

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -4,10 +4,14 @@ from django.contrib import admin
 
 from .models import Author, Genre, Book, BookInstance
 
+class BookInline(admin.TabularInline):
+    model = Book
 # Define the admin class
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+    inlines = [BookInline]
 
 # Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
@@ -27,6 +31,7 @@ class BookAdmin(admin.ModelAdmin):
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('status', 'due_back')
+    list_display = ('book', 'status', 'due_back', 'id')
 
     fieldsets = (
         (None, {
